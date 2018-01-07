b56b3042c992beb80fd47182f3d4f03b9a8c62da
dolidollsss <dolibeth.estevez@gmail.com>
2018-01-07 00:15:46 -0500

    Updated 2 die version of the game

--- twodice.js
+++ twodice.js
@@ -1,13 +1,14 @@
 /*
 GAME RULES:
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
+- In each turn, a player rolls two dice as many times as they wishe. Each result get added to thier ROUND score
+- BUT, if the player rolls a 1, all thier ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that thier ROUND score gets added to thier GLOBAL score.
+After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 var scores, roundScore, currentPlayer, prevRoll;
-var diceDOM = document.querySelector('.dice');
+var diceDOM = document.querySelector('.dice1');
 var gamePlaying = true;
 newGame();
 //want to set up an eventLitener for when the roll the dice button is clicked
@@ -15,24 +16,21 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
   if(gamePlaying){
     //need to calculate a random number for each die roll
     var dice = Math.floor(Math.random() * 6 + 1);
+    var dice2 = Math.floor(Math.random() * 6 + 1);
     //display the result
+    document.querySelector('.dice2').style.display = 'block';
     diceDOM.style.display = 'block';
+
     diceDOM.src = 'dice-' + dice + '.png';
+    document.querySelector('.dice2').src = 'dice-' + dice2 + '.png';
+
     //update the roundScore, only if die roll !== 1
-    if (dice !== 1) {
-      console.log("curr role: " + dice);
-      console.log("prev role: " + prevRoll);
-      if (prevRoll === 6 && dice === 6) {
-        scores[currentPlayer] = 0;
-        document.querySelector('#score-' + currentPlayer).textContent = '0';
-        nextPlayer();
-      }
-      roundScore += dice;
+    if (dice !== 1 && dice2 !== 1) {
+      roundScore = roundScore + dice + dice2;
       document.querySelector('#current-' + currentPlayer).textContent = roundScore;
     } else {
       nextPlayer();
     }
-    prevRoll = dice;
   }
 });
 
@@ -50,6 +48,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     if (scores[currentPlayer] >= 100) {
       document.querySelector('#name-' + currentPlayer).textContent = 'Winner!';
       diceDOM.style.display = 'none';
+      document.querySelector('.dice2').style.display = 'none';
       document.querySelector('.player-' + currentPlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + currentPlayer + '-panel').classList.remove('active');
       //document.querySelector('.btn-roll').addEventListener('click', newGame);
@@ -71,6 +70,7 @@ function nextPlayer() {
   document.querySelector('.player-0-panel').classList.toggle('active');
   document.querySelector('.player-1-panel').classList.toggle('active');
   diceDOM.style.display = 'none';
+  document.querySelector('.dice2').style.display = 'none';
 }
 
 function newGame(){
@@ -78,7 +78,7 @@ function newGame(){
   scores = [0, 0];
   roundScore = 0;
   currentPlayer = 0;
-
+  document.querySelector('.dice2').style.display = 'none';
   diceDOM.style.display = 'none';
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
